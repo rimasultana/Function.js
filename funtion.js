8eea33e9cbcce44bbcf247cf8201b6600971d6fa
rimasultana <rimasultana20225@gmail.com>
2024-09-05 22:07:04 +0600

    function shorkat

--- funtion.js
+++ funtion.js
@@ -8,15 +8,46 @@
 // doubleIt(30);
 // console.log('////////');
 
-function add(price1, price2) {
-  const total = price1 + price2;
-  return total;
-}
-const bill = add(5, 80);
-console.log(bill);
+// function add(price1, price2) {
+//   const total = price1 + price2;
+//   return total;
+// }
+// const bill = add(5, 80);
+// console.log(bill);
 
-function add2(price1, price2) {
-  return price1 + price2;
+// function add2(price1, price2) {
+//   return price1 + price2;
+// }
+// const bill2 = add2(5, 80);
+// console.log(bill, bill2);
+
+// function doMath(num1, num2) {
+//   const sum = num1 + num2;
+//   const diff = num1 - num2;
+//   const multiply = sum * diff;
+//   const result = multiply / 2;
+//   return result;
+// }
+// const result = doMath(10, 4);
+// console.log(result);
+
+// Even number
+
+// function isEven(number) {
+//   if (number % 2 === 0) {
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
+// console.log(isEven(5));
+// console.log(isEven(2));
+
+function isOdd(number){
+    if(number % 2 === 1){
+        return true;
+    }
+    return false;
 }
-const bill2 = add2(5, 80);
-console.log(bill, bill2);
+console.log(isOdd(5));
+console.log(isOdd(8));
