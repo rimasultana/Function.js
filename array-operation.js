bfa175c20fac17dc388314cb1e43c1e55c520388
rimasultana <rimasultana20225@gmail.com>
2024-09-06 11:36:31 +0600

    function length

--- array-operation.js
+++ array-operation.js
@@ -1,8 +1,9 @@
 function sumOfNumber(numbers){
+    let sum = 0;
     for(const number of numbers){
         console.log(number);
         sum = sum + number;
-    }return sum
+    }return sum;
 }
 const nums = [4, 2, 2, 6];
 const sum = sumOfNumber(nums);
