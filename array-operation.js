67f5f1c59bf174f867b95b1207aea0e1e95420fb
rimasultana <rimasultana20225@gmail.com>
2024-09-06 11:53:31 +0600

    odd and Even Number

--- array-operation.js
+++ array-operation.js
@@ -1,10 +1,38 @@
-function sumOfNumber(numbers){
-    let sum = 0;
+// function sumOfNumber(numbers){
+//     let sum = 0;
+//     for(const number of numbers){
+//         console.log(number);
+//         sum = sum + number;
+//     }return sum;
+// }
+// const nums = [4, 2, 2, 6];
+// const sum = sumOfNumber(nums);
+// console.log("Sum of number is: ", sum);
+
+
+
+// Even Number
+// function evenNumberOnly (numbers){
+// for(const number of numbers){
+//     if(number % 2 === 0){
+//         console.log(number);
+//     }
+    
+// }
+// }
+
+// const numbers = [5, 8, 91, 24, 6];
+// evenNumberOnly(numbers);
+
+// Odd Number
+
+function oddNumberOnly (numbers){
     for(const number of numbers){
-        console.log(number);
-        sum = sum + number;
-    }return sum;
+        if(number % 2 === 1){
+            console.log(number);
+        }
+
+    }
 }
-const nums = [4, 2, 2, 6];
-const sum = sumOfNumber(nums);
-console.log("Sum of number is: ", sum);
+const numbers = [5, 8, 91, 24, 6];
+oddNumberOnly(numbers)
